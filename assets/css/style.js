7fd9182fb1effc461697b4341db0ccc5f041bfb6
T-bag404 <hamzakouissi3@gmail.com>
2025-01-31 12:55:04 +0100

    this is GI

---
+++ assets/css/style.js
@@ -0,0 +1,9 @@
+// carousel.js
+let currentIndex = 0;
+const items = document.querySelectorAll('.carousel-item');
+
+function moveCarousel(step) {
+    items[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + step + items.length) % items.length;
+    items[currentIndex].classList.add('active');
+}
